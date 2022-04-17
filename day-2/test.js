23e9447857f82c211735a9c09d6dd6ba16472f4b
Satyam Saw <satyamkumarsaw30@gmail.com>
2022-04-17 20:15:11 +0530

    Write a program to reverse a string
    Write a program to reverse a string

---
+++ day-2/test.js
@@ -0,0 +1,15 @@
+const str = "JavaScript is awesome";
+
+function reverseAString(str) {
+  // write your solution here
+  let newString = "";
+
+  for (let i = str.length -1 ; i >= 0; i--) {
+      newString = newString + str[i]
+
+  }
+
+  return newString;
+}
+
+console.log(`Reversed string is: ${reverseAString(str)}`);
